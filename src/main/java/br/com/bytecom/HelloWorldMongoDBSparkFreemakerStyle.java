ea65aff8dca6c20d6c7341c6b40083d35d63ec06
clairtonluz <clairton.c.l@gmail.com>
2014-01-11 12:08:04 -0300

    All togheter MongoDB, Spark and Freemaker

---
+++ src/main/java/br/com/bytecom/HelloWorldMongoDBSparkFreemakerStyle.java
@@ -0,0 +1,61 @@
+package br.com.bytecom;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+/**
+ *
+ * @author clairton
+ */
+public class HelloWorldMongoDBSparkFreemakerStyle {
+
+    public static void main(String args[]) {
+        final Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(HelloWorldFreemakerStyle.class, "/");
+
+        Spark.get(new Route("/") {
+
+            @Override
+            public Object handle(Request rqst, Response rspns) {
+
+                StringWriter writer = new StringWriter();
+                try {
+                    MongoClient client = new MongoClient("localhost", 27017);
+
+                    DB database = client.getDB("bytecom");
+                    DBCollection collection = database.getCollection("client");
+
+                    DBObject document = collection.findOne();
+
+                    Template helloTemplate = configuration.getTemplate("hello.ftl");
+                    Map<String, Object> helloMap = new HashMap<String, Object>();
+                    helloMap.put("name", document.get("name"));
+
+                    helloTemplate.process(helloMap, writer);
+
+                } catch (IOException ex) {
+                    Logger.getLogger(HelloWorldFreemakerStyle.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (TemplateException ex) {
+                    Logger.getLogger(HelloWorldFreemakerStyle.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                return writer;
+            }
+        });
+
+    }
+}
